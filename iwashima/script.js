c191c74f1466694a7e863d1a5ee7e1d2500fb788
YuIwashima <32728975+YuIwashima@users.noreply.github.com>
2018-06-13 18:30:41 +0900

    Update script.js

--- iwashima/script.js
+++ iwashima/script.js
@@ -1,10 +1,12 @@
 const medias = {audio : false, video : {
-	facingMode : {
-	exact : "environment"
-	}
-}
-	video  = document.getElementById("video");
-	navigator.getUserMedia(medias, successCallback, errorCallback);
+        facingMode : {
+          exact : "environment"
+        }
+      }},
+      video  = document.getElementById("video");
+
+navigator.getUserMedia(medias, successCallback, errorCallback);
+
 function successCallback(stream) {
   video.srcObject = stream;
 };
