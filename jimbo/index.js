734d280b1211c0b92d0e3084c8cf238cec73d7a0
神保悟 <32728985+JinboSatoru@users.noreply.github.com>
2018-06-14 10:07:46 +0900

    Add files via upload

--- jimbo/index.js
+++ jimbo/index.js
@@ -5,39 +5,74 @@ const medias = {audio : false, video : {
       }},
       video  = document.getElementById("video");
 
+// const medias = {audio : false, video: true};
+
 navigator.getUserMedia(medias, successCallback, errorCallback);
-
-
-
-
-var canvas = document.getElementById('canvas');
-if (canvas.getContext) {
-
-    var context = canvas.getContext('2d');
-
-    //左から20上から40の位置に、幅50高さ100の四角形を描く
-    context.fillRect(20,40,50,100);
-
-    //色を指定する
-    context.strokeStyle = 'rgb(00,00,255)'; //枠線の色は青
-    context.fillStyle = 'rgb(255,00,00)'; //塗りつぶしの色は赤
-
-    //左から200上から80の位置に、幅100高さ50の四角の枠線を描く
-    context.strokeRect(200,80,100,50);
-
-    //左から150上から75の位置に、半径60の半円を反時計回り（左回り）で描く
-    context.arc(150,75,60,Math.PI*1,Math.PI*2,true);
-    context.fill();
-
-} 
-
+var cvs = document.createElement('canvas');
 
 
 
 function successCallback(stream) {
-  video.srcObject = stream;
+    video.srcObject = stream;
+
+    video.onloadedmetadata = function(e) {
+
+        
+        var websocket = new WebSocket("ws://10.73.8.152:8000/websocket");
+
+        // When the connection is open, send some data to the server
+        websocket.onopen = function () {
+
+            //https://javascript.programmer-reference.com/js-image-base64/
+            //canvas要素を生成してimg要素を反映する
+            cvs.width  = video.videoWidth;
+            cvs.height = video.videoHeight;
+
+            function sendData(){
+                // websocket.send(video);
+                //while(true){
+                var ctx = cvs.getContext('2d');
+                ctx.drawImage(video, 0, 0);
+
+                //canvas要素をBase64化する
+                var data = cvs.toDataURL("image/jpeg");
+
+                websocket.send(data);
+
+                delete ctx;
+                delete data;
+            }
+            setInterval(sendData, 100);
+        };
+
+        // Log errors
+        websocket.onerror = function (error) {
+            console.log('WebSocket Error ' + error);
+        };
+
+        // Log messages from the server
+        websocket.onmessage = function (e) {
+            //https://stackoverflow.com/questions/8473205/convert-and-insert-base64-data-to-canvas-in-javascript
+            var c = document.getElementById("canvasResult");
+            var ct = c.getContext("2d");
+
+            var image = new Image();
+
+            image.onload = function() {
+                c.width = image.width;
+                c.height = image.height;            
+                ct.drawImage(image, 0, 0);
+            };
+            image.src = "data:image/jpeg;base64," + e.data;
+            delete c;
+            delete ct;
+            delete e;
+            delete image;
+        };
+    };
+    
 };
 
 function errorCallback(err) {
-  alert(err);
+    alert(err);
 };
